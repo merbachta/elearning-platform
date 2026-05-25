fafac196f154b5a429c51dc7c67efe826d3f7c85
merbachta <derienmercidesole@gmail.com>
2026-05-25 10:18:41 +0200

    Día 6 - Finalización BACKEND (3 bloques / 6)

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Task, Submission
+from django.utils import timezone #AÑADIMOS ESTA IMPORTACIÓN
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,9 +8,20 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'deadline', 'created_by', 'created_at']
         read_only_fields = ['created_by', 'created_at']
 
+    def validate_deadline(self, value):  #AÑADIMOS ESTE MÉTODO
+        if value < timezone.now():
+            raise serializers.ValidationError("The deadline cannot be in the past.")
+        return value   
+
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ['id', 'task', 'student', 'file', 'submitted_at', 'status', 'grade', 'feedback']
         read_only_fields = ['student', 'submitted_at']
+    
+    def validate(self, data): #AÑADIMOS ESTE MÉTODO
+        task = data.get('task')
+        if task and task.deadline < timezone.now():
+            raise serializers.ValidationError("Cannot submit a task past its deadline.")
+        return data
 

242031af7b569984d6ee00c40e3f4777256957fa
merbachta <derienmercidesole@gmail.com>
2026-06-08 09:07:44 +0200

    Actualizacion core/settings.py para cambiar static url.

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CORS - permite comunicación con el frontend React
 CORS_ALLOWED_ORIGINS = [
